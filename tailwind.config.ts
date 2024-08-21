041df2937320da85e667f1be9fd363a8c0d0bf08
unknown <dohouint@gmail.com>
2024-08-21 09:23:07 +0200

    Update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,24 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        twhite: '#FFFFFF',
+        tlightgray : '#F3F4F6',
+        tgray : '#D1D5DB',
+        tdarkgray : '#6B7280',
+        tdark : '#1D1D1D',
+        tyellow : '#F2D604',
+        tgreen : '#01B130',
+        tred : '#CB0101',
+        tblue : '#006EFF',
+        tdark2 : '#0065EA',
+        tdarkblue1 : '#171923',
+        tdarkblue2 : '#292C3B',
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
