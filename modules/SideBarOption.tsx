b6a79b8f21b8e9e71808ae0c2bc81ab03bbfa1aa
unknown <dohouint@gmail.com>
2024-08-26 19:19:24 +0200

    Update

--- modules/SideBarOption.tsx
+++ modules/SideBarOption.tsx
@@ -5,7 +5,7 @@ type Props = {text:string};
 const SideBarOption: React.FC<Props> = ({ text }) => {
   return (
     <div className='w-full p-3 hover:bg-tblue flex gap-3 cursor-pointer items-center'>
-      <div className='w-[100px] h-[100px] ring-1 ring-tdark'></div>
+      <div className='w-[18px] h-[18px] ring-1 ring-tdark'></div>
       <a className='text-tdark text-xs font-semibold'>{text}</a>
     </div>
   )
